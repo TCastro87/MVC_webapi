0c707dcf5ebeb67600c593c6b7a9af0151d672b5
tiagocastro <tiago_castro@outlook.com>
2018-10-02 11:32:43 -0300

    Initial commit

---
+++ MVC_webapi/Models/Livro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_webapi.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
